92cdc938efb4526315d9c2a8a93e76b4eaaa2e76
luqihua <luqihua1990@gmail.com>
2017-07-04 09:18:52 +0800

    fix cookie store class

--- RxHttp/src/main/java/com/lu/obj/PersistentCookieStore.java
+++ RxHttp/src/main/java/com/lu/obj/PersistentCookieStore.java
@@ -59,7 +59,7 @@ public class PersistentCookieStore {
         String name = getCookieToken(cookie);
 
         //将cookies缓存到内存中 如果缓存过期 就重置此cookie
-        if (cookie.persistent()) {
+        if (!cookie.persistent()) {
             if (!cookies.containsKey(url.host())) {
                 cookies.put(url.host(), new ConcurrentHashMap<String, Cookie>());
             }
